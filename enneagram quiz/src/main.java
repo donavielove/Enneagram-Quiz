6eba00216fae62e0ad0d843e212ef2bbbcb754d1
Donavie Ordonez <47704302+donavielove@users.noreply.github.com>
2020-07-10 16:44:10 -0700

    Add files via upload

---
+++ enneagram quiz/src/main.java
@@ -0,0 +1,10 @@
+/*
+ * This main class allows the user to see the 
+ * Enneagram Test GUI
+ */
+public class main {
+	public static void main(String[] args) {
+		enneagramTest frame = new enneagramTest();
+		frame.setVisible(true);
+	}
+}
